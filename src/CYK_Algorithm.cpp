f8a9ad241906a59d44e4e7141dddfab54882a619
Daniil <daniil31415it@gmail.com>
2023-11-06 18:45:49 +0300

    Fixed cache misses, made small renaming

--- src/CYK_Algorithm.cpp
+++ src/CYK_Algorithm.cpp
@@ -45,12 +45,11 @@ namespace fl::cyk {
 
             for (size_t len = 0; len <= text.size(); ++len) {
                 for (size_t pos = 0; pos + len <= text.size(); ++pos) {
-                    for (const auto& [nt_code, rrs_vec] : terminal_multirules) {
-                        for (const auto& rrs : rrs_vec) {
-                            // todo: reduce cache misses by changing the order of indices
-                            v[nt_table[nt_code]][len][pos] = isTerminalRecognized(text_sv.substr(pos, len), rrs);
+                    for (const auto& [nt_code, multirrs] : terminal_multirules) {
+                        for (const auto& rrs : multirrs) {
+                            v[len][pos][nt_table[nt_code]] = isTerminalRecognized(text_sv.substr(pos, len), rrs);
 
-                            if (v[nt_table[nt_code]][len][pos]) {
+                            if (v[len][pos][nt_table[nt_code]]) {
                                 break;
                             }
                         }
@@ -67,12 +66,12 @@ namespace fl::cyk {
 
         NonterminalTokenKeyTable nt_table;
         initNonterminalTokenKeyTable(nt_table, g);
-        // is_recognized[nt_table[nt_code]][length][position] == true,
+        // is_recognized[length][position][nt_table[nt_code]] == true,
         // if there is an output for the grammar to text[position:position + length]
         // that starts from nt_code
-        std::vector<SubstringVector> is_recognized(g.tntable.nt_count,
-                                                   SubstringVector(text.size() + 1,
-                                                                   std::vector<bool>(text.size(), false)));
+        std::vector<SubstringVector> is_recognized(text.size() + 1,
+                                                   SubstringVector(text.size(),
+                                                                   std::vector<bool>(g.tntable.nt_count, false)));
         initRecognitionVector(is_recognized, text, nt_table, g);
 
         size_t nt_code;
@@ -94,20 +93,20 @@ namespace fl::cyk {
                         // the rule looks like A -> BC
                         a_nt_code = nt_table[rrs.sequence[0]];
                         b_nt_code = nt_table[rrs.sequence[1]];
-                        is_recognized[nt_code][len][pos] = is_recognized[a_nt_code][len][pos] &&
-                                is_recognized[b_nt_code][0][0];
+                        is_recognized[len][pos][nt_code] = is_recognized[len][pos][a_nt_code] &&
+                                is_recognized[0][pos][b_nt_code];
 
                         for (size_t k = 0; !is_recognized[nt_code][len][pos] && k < len; ++k) {
-                            is_recognized[nt_code][len][pos] =
-                                    is_recognized[a_nt_code][k][pos] &&
-                                    is_recognized[b_nt_code][len - k][pos + k];
+                            is_recognized[len][pos][nt_code] =
+                                    is_recognized[k][pos][a_nt_code] &&
+                                    is_recognized[len - k][pos + k][b_nt_code];
                         }
                     }
                 }
             }
         }
 
-        return is_recognized[nt_table[g.start]][text.size()][0];
+        return is_recognized[text.size()][0][nt_table[g.start]];
     }
 
 }  // namespace fl::cyk
